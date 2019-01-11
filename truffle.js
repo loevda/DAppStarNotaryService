e027aba045e6b0479d781f7fc55b764760feddda
david2099 <loevenbruck.david@gmail.com>
2019-01-11 21:26:28 +0100

    refactor: added .env file

--- truffle.js
+++ truffle.js
@@ -1,15 +1,20 @@
 // Allows us to use ES6 in our migrations and tests.
-require('babel-register')
+require('dotenv').config();
+require('babel-register');
+require('babel-polyfill');
+
+const infuraKey = process.env.INFURA_KEY;
+const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 // Edit truffle.config file should have settings to deploy the contract to the Rinkeby Public Network.
 // Infura should be used in the truffle.config file for deployment to Rinkeby.
 
 module.exports = {
-  networks: {
-    ganache: {
-      host: '127.0.0.1',
-      port: 7545,
-      network_id: '*' // Match any network id
+    networks: {
+        ganache: {
+            host: '127.0.0.1',
+            port: 7545,
+            network_id: '*' // Match any network id
+        }
     }
-  }
 }
